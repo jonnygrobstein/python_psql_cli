9dcc2460aada0768df1789876fc401c606b059c9
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-04-22 14:20:47 -0500

    done

--- lib/notes.py
+++ lib/notes.py
@@ -21,8 +21,6 @@ class Note(BaseModel):
     title = CharField(max_length=255)
     notes = TextField()
     is_priority = BooleanField(default=False)
-    due_date = DateField(null=True)
-    created_at = TimestampField()
     
     def __str__(self):
         return f'{self.title}: {self.notes[:50]}'
@@ -37,52 +35,55 @@ def add_note():
     title = input('Enter the note title: ')
     notes = input('Enter the note content: ')
     is_priority = input('Is this note a priority? (y/n): ').lower() == 'y'
-    due_date = input('Enter the due date (YYYY-MM-DD) (optional): ')
-    if due_date:
-        due_date = datetime.datetime.strptime(due_date, '%Y-%m-%d').date()
-        
-        try:
-            note = Note.create(title=title, notes=notes, is_priority=is_priority, due_date=due_date)
-            print(f'New note created titled {title}.')
-        except IntegrityError:
-            print(f'Note with title {title} already exists in the database.')
+    try:
+        note = Note.create(title=title, notes=notes, is_priority=is_priority)
+        print(f'New note created titled {title}.')
+        conn.commit()
+        # note.save()
+    except IntegrityError:
+        DATABASE.rollback()
+        note = Note.get(title=title)
+        print(f'Note with title {title} already exists in the database.')
+    return note
 
-# function to list the notes in the database and return them as a dictionary
+# function to list the notes in the database and return them as a dictionary mapping the ID to the note object
 def get_notes():
     notes = Note.select()
     if notes.count() > 0:
         note_dict = {}
         for note in notes:
-            note_dict[note.id] = note
-            print(f'{note.id}: {note.title}')
+            note.id_str = str(note.id) # convert the ID to a string and store it as an attribute of the note object
+            note_dict[note.id_str] = note
+            print(f'{note.id_str}: {note.title}')
         return note_dict
     else:
         print('No notes found in the database.')
         return None
     
 # function to update a note in the database
-def update_note():
-    id = input('Enter the ID of the note you want to edit: ')
-    title = input('Enter the updated title (press enter to keep current title): ')
-    notes = input('Enter the updated notes (press enter to keep current notes): ')
-    is_priority = input('Is this note a priority? (y/n): ').lower() == 'y'
-    due_date = input('Enter the due date (YYYY-MM-DD) (press enter to keep current due date): ')
-    if due_date:
-        due_date = datetime.datetime.strptime(due_date, '%Y-%m-%d').date()
-    try:
-        note = Note.get(Note.id == id)
-        if title:
-            note.title = title
-        if notes:
-            note.notes = notes
-        if is_priority:
+def update_note(note_dict):
+    notes = get_notes()
+    if notes:
+        selection = input('Enter the number of the note you want to edit: ')
+        try:
+            note = notes[selection] # search for a note with the given ID
+        except KeyError:
+            print('Note not found.')
+            return
+        title = input(f'Enter the updated title (press enter to keep current title "{note.title}"): ')
+        notes = input(f'Enter the updated notes (press enter to keep current notes "{note.notes}"): ')
+        is_priority = input(f'Is this note a priority? (y/n) (current priority: {note.is_priority}): ').lower() == 'y'
+        
+        try:
+            if title:
+                note.title = title
+            if notes:
+                note.notes = notes
             note.is_priority = is_priority
-        if due_date:
-            note.due_date = due_date
-        note.save()
-        print(f'Note with id {id} updated in the database.')
-    except DoesNotExist:
-        print(f'Note with id {id} does not exist in the database.')
+            note.save()
+            print(f'Note with title "{note.title}" updated in the database.')
+        except IntegrityError:
+            print(f'Note with title "{title}" already exists in the database.')
 
 # function to list the notes in the database
 def list_notes():
@@ -107,44 +108,50 @@ def search_notes():
         print('No notes found matching the query.')
 
 def main():
-    while True:
-        # show the main menu
-        print('What would you like to do?')
-        print('1. Add a note')
-        print('2. Edit a note (by ID)')
-        print('3. List all notes')
-        print('4. Search notes')
-        print('5. Quit')
+    with conn:
+        if not DATABASE.is_closed():
+            DATABASE.close()
+        DATABASE.connect()
+        while True:
+            # show the main menu
+            print('What would you like to do?')
+            print('1. Add a note')
+            print('2. Edit a note (by ID)')
+            print('3. List all notes')
+            print('4. Search notes')
+            print('5. Quit')
 
-        # get the user's choice
-        choice = input('Enter the number of your choice: ')
+            # get the user's choice
+            choice = input('Enter the number of your choice: ')
 
-        # handle the user's choice
-        if choice == '1':
-            # add a note
-            add_note()
-        elif choice == '2':
-            # edit a note
-            note_dict = get_notes()
-            if note_dict:
-                note_id = input('Enter the ID of the note you want to edit: ')
-                if note_id in note_dict:
-                    edit_note(note_dict[note_id])
-                else:
-                    print(f'Note with ID {note_id} not found.')
-        elif choice == '3':
-            # list all notes
-            get_notes()
-        elif choice == '4':
-            # search notes
-            search_notes()
-        elif choice == '5':
-            # quit the program
-            print('Goodbye!')
-            break
-        else:
-            print('Invalid choice. Please enter a number from 1 to 5.')
+            # handle the user's choice
+            if choice == '1':
+                # add a note
+                add_note()
+            elif choice == '2':
+                # edit a note
+                note_dict = get_notes()
+                if note_dict:
+                    note_id = input('Enter the ID of the note you want to edit: ')
+                    if note_id in note_dict:
+                        update_note(note_dict[note_id])
+                    else:
+                        print(f'Note with ID {note_id} not found.')
+            elif choice == '3':
+                # list all notes
+                get_notes()
+            elif choice == '4':
+                # search notes
+                search_notes()
+            elif choice == '5':
+                # quit the program
+                print('Goodbye!')
+                break
+            else:
+                print('Invalid choice. Please enter a number from 1 to 5.')
 
+if not DATABASE.is_closed():
+    DATABASE.close()
 
 if __name__ == '__main__':
     main()
--- lib/schema.sql
+++ lib/schema.sql
@@ -3,10 +3,8 @@ DROP TABLE IF EXISTS Notes;
 
 CREATE TABLE Notes (
   id SERIAL PRIMARY KEY,
-  created_at timestamp not null default CURRENT_TIMESTAMP,
-  updated_at timestamp not null default CURRENT_TIMESTAMP,
-  title VARCHAR(255) NOT NULL,
+  updated_at timestamp without time zone not null default CURRENT_TIMESTAMP,
+  title VARCHAR(255) NOT NULL,  
   notes TEXT NOT NULL,
   is_priority BOOLEAN DEFAULT FALSE,
-  due_date DATE
 );
